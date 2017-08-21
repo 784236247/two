615d06108cbb826673dcb356a13c0e38a977b2a5
zdl <zdl@666.om>
2017-08-21 17:20:22 +0800

    撒旦撒范德萨飞洒发斯蒂芬

--- testgit/src/com/test/Test.java
+++ testgit/src/com/test/Test.java
@@ -3,8 +3,7 @@ package com.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello Git");
-		System.out.println("hello Git");
-		System.out.println("hello Git");
+		
 	}
 
 }
