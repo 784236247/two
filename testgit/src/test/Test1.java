1751fa6c8ecd61d4f64c97f07ad78e63142707e8
zdl <zdl@666.om>
2017-08-21 19:12:31 +0800

    int a = 10

--- testgit/src/test/Test1.java
+++ testgit/src/test/Test1.java
@@ -2,6 +2,7 @@ package test;
 
 public class Test1 {
 
+	int a= 10; 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("https://github.com/784236247/two.git");
