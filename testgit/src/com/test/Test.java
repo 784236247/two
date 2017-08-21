071564aaaf950d1cbe6ba51a01133af8e1378072
zdl <zdl@666.om>
2017-08-21 17:39:28 +0800

    第三方发反反复复反复反复反复反复

--- testgit/src/com/test/Test.java
+++ testgit/src/com/test/Test.java
@@ -5,9 +5,8 @@ public class Test {
 		int y = 9;
 	public static void main(String[] args) {
 
-		System.out.println("hello Git");
-		System.out.println("hello Git");
-		System.out.println("hello Git");
+
+		System.out.println("修改了");
 		System.out.println("hello Git");
 		System.out.println("hello Git");
 		
